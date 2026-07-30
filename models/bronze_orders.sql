861f578378c0da473cd3093d5ebdd090482f776d
Nandhana Prakash <nandhanap100@gmail.com>
2026-07-30 02:02:30 +0000

    added landing source yml

--- models/bronze_orders.sql
+++ models/bronze_orders.sql
@@ -1,1 +1,5 @@
-select * from dbt_project_catalog.landing.orders
+select * from 
+/* could do below too but since sources is defined in landing_sources.yml ill ref that
+dbt_project_catalog.landing.products */
+{{source('landing', 'orders')}}
+/* meaning inside landing  sources , orders table */
